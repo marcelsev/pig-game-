bec1ba9865e5568877760eff543aa7ec9fbeb8b7
marcelsev <marcelosolanosev@gmail.com>
2023-03-27 14:50:18 +0200

    function js

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const diceEl = document.querySelector("#dice");
+const score1El =document.querySelector("#score--1");
+const score2El= document.querySelector("#score--2");
+const btnRoll = document.querySelector("#btn--roll");
+const btnNew = document.querySelector("#btn--new");
+const btnHold= document.querySelector("#btn--hold");
+const currentScoreEl1= document.querySelector("#current--score--1");
+const currentScoreEl2= document.querySelector("#current--score--2");
+const player1El= document.querySelector("#player--1");
+const player2El= document.querySelector("#player--2");
+
+//initialitation
+
+diceEl.src = "";
+score1El.textContent = 0;
+score2El.textContent = 0;
+let currentScore= 0;
+let currentPlayer= 1;
+let scorePlayer1= 0;
+let scorePlayer2= 0;
+let play= true;
+
+btnRoll.addEventListener("click", function(){
+const dice = Math.trunc(Math.random()*6+1);
+
+diceEl.src= `./images/dice${dice}.png`;
+
+if(dice===1){
+    currentPlayer = currentPlayer === 1?2:1;
+    currentScore= 0;
+    currentScoreEl1.textContent= 0;
+    currentScoreEl2.textContent=0;
+    player1El.classList.toggle("active");
+    player2El.classList.toggle("active");
+    return;
+}
+currentScore += dice;
+if(currentPlayer === 1){
+    currentScoreEl1.textContent= currentScore;
+}else{
+    currentScoreEl2.textContent= currentScore;
+}
+})
+
+btnHold.addEventListener("click", function(){
+    if (play === false) return;
+
+    if (currentPlayer===1 ){
+        scorePlayer1 += currentScore;
+        score1El.textContent=scorePlayer1;
+        winner(scorePlayer1);
+
+    }else{
+        scorePlayer2+= currentScore;
+        score2El.textContent= scorePlayer2;
+        winner(scorePlayer2);
+    }
+});
+const winner= (score)=>{
+    if(score>= 20){
+        if (currentPlayer === 1){
+            player1El.classList.add("winner");
+        }else{
+            player2El.classList.add("winner");
+        }
+        currentScoreEl1.textContent= 0;
+        currentScoreEl2.textContent= 0;
+        diceEl.src= "";
+        play= false;
+    }else {
+        switchPlayer();
+    }
+};
+const switchPlayer = ()=>{
+    currentPlayer=currentPlayer=== 1?2:1;
+    currentScore= 0;
+currentScoreEl1.textContent=0;
+currentScoreEl2.textContent=0;
+player1El.classList.toggle("active");
+player2El.classList.toggle("active");
+};
